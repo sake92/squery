23fc50ca3f6087f42c32ffdcbd73f44731143680
Sakib Hadziavdic <sakib@sake.ba>
2025-03-24 00:06:53 +0100

    Fix docs

--- docs/src/utils/Consts.scala
+++ docs/src/utils/Consts.scala
@@ -6,7 +6,7 @@ object Consts:
 
   val ArtifactOrg = "ba.sake"
   val ArtifactName = "squery"
-  val ArtifactVersion = "0.7.0"
+  val ArtifactVersion = "0.6.6"
 
   val GhHandle = "sake92"
   val GhProjectName = "squery"
